23f46153fabe25fe0c44f5cf5a79bfb6523a1d86
Josiah <josiah_g@live.com>
2023-01-20 23:52:19 -0500

    Update index.js
    Finished

--- Lab1/index.js
+++ Lab1/index.js
@@ -8,8 +8,39 @@ console.log(a[1])
 console.log(a[2])
 console.log(a.length)
 
-a[0] = 97
+a[0] = 88
 console.log(a.toString())
 // for (var i = 0; i < a.length; i++) {
 //     console.log(a[i])
 // }
+
+var data = "😀😀"
+var buf = Buffer.from(data)
+
+console.log(buf)
+console.log(buf.toString())
+console.log(buf.length)
+console.log(buf[0])
+
+var values = "©️©️"
+var buf2 = Buffer.from(values)
+console.log(buf2)
+console.log(buf2.toString())
+console.log(buf2.length)
+
+// using alloc
+ var buf3 = Buffer.alloc(10, "HELLO")
+ console.log(buf3.toString())
+
+
+ var buf4 = Buffer.alloc(15)
+ buf4.write("Hello")
+ console.log(buf4.toString())
+
+
+ // Concate Buffer
+
+ var newBuffer = a + buf
+    console.log(newBuffer)
+    newBuffer = Buffer.concat([a, buf, buf2])
+    print = newBuffer.toString()
